d516cd78c4fd06202c44c7ffbd0fa4d0b5013bf2
madhurad <madhura.deshpande@cloudchainsoftware.com>
2019-05-08 12:24:55 +0400

    Temperature display in temperature div

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
 $(function () {
 
+    let searchButton = document.getElementById('searchCityBtn');
+    let searchInput = document.getElementById('inputCity');
+    let temperature = document.getElementById("tempDiv");
+
+    const key = "adcbaf2ac33077b45b05846355cc474f";
+
+    searchButton.addEventListener("click", getWeatherDetails);
+    searchInput.addEventListener("keyup", enterPressed);
+
+    function enterPressed(event) {
+        if (event.key == "Enter") {
+            getWeatherDetails();
+        }
+        else {
+
+        }
+    }
+
+    function getWeatherDetails() {
+        if (searchInput.value == "") {
+            alert ('Please enter City name');
+        }else {
+            let url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+key;
+            console.log(url);
+            httpRequestAsync(url, urlResponse);
+        }
+    }
+
+    function urlResponse(response) {
+        let jsonObject = JSON.parse(response);
+        temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + " C";
+    }
+
+    function httpRequestAsync(url, callback){
+        let httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = () => { 
+            if (httpRequest.readyState == 4 && httpRequest.status == 200)
+                callback(httpRequest.responseText);
+        }
+        httpRequest.open("GET", url, true);
+        httpRequest.send();
+    }
 });
